1c6bdbb47d0996c31521a15b93d13a04a6f0b504
jjaniak <julia.janiak@gmail.com>
2019-05-31 13:56:42 +0200

    remove comments, add and instantiate InputResetEmail, correct typeInvalidEmail method

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,6 +1,4 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.Assert.assertEquals;
@@ -11,31 +9,19 @@ public class LoginPage extends Page {
     private ButtonLogin buttonLogin = new ButtonLogin(driver, wait);
     private ButtonForgetPassword buttonForgetPassword = new ButtonForgetPassword(driver, wait);
     private ButtonReset buttonReset = new ButtonReset(driver, wait);
-    private ButtonCookie buttonCookie = new ButtonCookie(driver, wait);
+    private ButtonGotIt buttonGotIt = new ButtonGotIt(driver, wait);
     private InputEmail inputEmail = new InputEmail(driver, wait);
     private InputPassword inputPassword = new InputPassword(driver, wait);
+    private InputResetEmail inputResetEmail = new InputResetEmail(driver, wait);
 
 
     public LoginPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
 
-//    private By inputEmail = By.name("username");
-//
-//    private void typeText(String email) {
-//        inputEmail.typeText(email);
-//    }
-//
-//     private By inputPassword = By.name("password");
-//
-//    private void typePassword(String password) {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(inputPassword));
-//        driver.findElement(inputPassword).sendKeys(password);
-//    }
-
     private void clickLogin() {
-        buttonLogin.click();}       // oneliner like here?
-
+        buttonLogin.click();
+    }
 
     public void loginWith(String email, String password) {
         inputEmail.typeText(email);
@@ -47,20 +33,16 @@ public class LoginPage extends Page {
         assertEquals(message, error.getText());
     }
 
-
     public void closePopup() {
-        buttonCookie.click();
+        buttonGotIt.click();
     }
 
     public void clickForgetPassword() {
         buttonForgetPassword.click();
     }
 
-    private By resetEmailSelector = By.cssSelector("input[id='resetemail']");
-
-    public void typeInvalidEmail(String email) {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(resetEmailSelector));
-        driver.findElement(resetEmailSelector).sendKeys(email);
+    public void typeInvalidEmail(String invalidEmail) {
+        inputResetEmail.typeText(invalidEmail);
     }
 
     public void clickResetButton() {buttonReset.click();
